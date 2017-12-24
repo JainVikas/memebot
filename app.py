6eec3c7524101326d61d081d0d29d61ede76ec6f
Vikas Jain <vikasjain081291@live.com>
2017-12-24 16:46:54 +0530

    debug comments

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def images():
     }
 
     for i in range(9):
-        count=i + int(inputRecords)
+        i=i + int(inputRecords)
         resultTemplate={
                "buttons": [
                 {
@@ -112,7 +112,7 @@ def images():
            "buttons": [
             {
               "set_attributes": {
-                "inputRecords": count+1,
+                "inputRecords": i+1,
               },
               "block_names": [
                 "imageJson"
