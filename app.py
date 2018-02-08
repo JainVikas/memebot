184caeb8011778b0c0e1a0fd2c31a7ed218618c4
Vikas Jain <vikasjain081291@live.com>
2018-02-08 20:53:49 +0530

    with cloudinary API

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ def generateMeme():
         if 'jpg' in imageInput:
             result = {"messages": [{"attachment": {"type": "image","payload": { "url": str(cloudinaryImage(upperText,lowerText,imageInput))}}}]}
         else:
-            result = {"messages":[ {"text": "well, i needed an image, not this."}],{"redirect_to_blocks": ["userImage"]}}
+            result = {"messages":[ {"text": "well, i needed an image, not this."},{"redirect_to_blocks": ["userImage"]}]}
     return jsonify(result)
 if __name__ == '__main__':
   app.debug = True
